1cce5c9f33f7ad0a39a6d2fbba6af5b75088c04e
Kartikey Chauhan <Kartikey15@gmail.com>
2025-05-15 09:32:50 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -5,10 +5,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 
 connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+app.use("/user",userRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello world");
