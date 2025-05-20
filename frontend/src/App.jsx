3661f69b28092667c3e69ef613e54ef8fc1168bd
Kartikey Chauhan <Kartikey15@gmail.com>
2025-05-20 18:01:08 +0530

    user UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import UserProtectedWrapper from './pages/UserProtectedWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import Riding from './pages/Riding'
 
 const App = () => {
 
@@ -21,6 +22,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Start/>} />
         <Route path="/login" element={<UserLogin/>} />
+        <Route path="/riding" element={<Riding/>} />
         <Route path="/signup" element={<UserSignup/>} />
         <Route path="/captain-login" element={<Captainlogin/>} />
         <Route path="/captain-signup" element={<CaptainSignup/>} />
